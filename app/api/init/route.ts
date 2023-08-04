09e5ac82c66d16c56284af6e02ed4855b3656a85
taimoorM <taimoor.moh@gmail.com>
2023-08-04 13:03:34 -0400

    Update: init api

--- app/api/init/route.ts
+++ app/api/init/route.ts
@@ -1,11 +1,31 @@
-import { NextRequest } from "next/server";
+import { supabase } from "@/libs/supabase";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   console.log("GET /api/init");
 
-  const subdomain = req.headers.get("bb-subdomain");
-  console.log("subdomain", subdomain);
+  const subdomain = req.headers.get("bb-subdomain")?.toLocaleLowerCase();
 
   try {
-  } catch (e) {}
+    const { data, error } = await supabase
+      .from("Customer")
+      .select()
+      .eq("subdomain", subdomain);
+
+    if (error) {
+      throw error;
+    }
+
+    if (!data.length) {
+      return NextResponse.json(
+        { error: "No subdomain found" },
+        { status: 400 }
+      );
+    }
+
+    console.log("data", data);
+  } catch (e) {
+    console.log(e);
+    NextResponse.json({ error: e }, { status: 500 });
+  }
 }
