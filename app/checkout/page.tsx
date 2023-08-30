2f6060ac9b7f15013197bea6ab0ba83aa387502e
taimoorM <taimoor.moh@gmail.com>
2023-08-30 16:54:14 -0400

    Update: checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -6,20 +6,30 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 export default function CheckoutPage() {
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+  );
+
   const { headers } = useStore();
   const { data } = useQuery({
     queryKey: ["checkout"],
     queryFn: () => {
       return fetch("api/storefront/checkout", {
         headers,
-      });
+      }).then((res) => res.json());
     },
   });
+
+  const options = {
+    clientSecret: data?.clientSecret as string,
+  };
 
   return (
     <div>
       <h1>Checkout</h1>
+      <Elements options={options} stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 }
-``;
